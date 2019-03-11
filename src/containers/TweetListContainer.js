6090461c2c56bbd2e9c014e8f8a30d8f2f0e4836
Hugo Felkel <felkelhl@g.cofc.edu>
2019-03-10 20:52:45 -0400

    Minor text fix

--- src/containers/TweetListContainer.js
+++ src/containers/TweetListContainer.js
@@ -9,6 +9,6 @@ const mapStateToProps = state => ({
 
 /**
  * https://redux.js.org/recipes/using-immutablejs-with-redux#use-a-higher-order-component-to-convert-your-smart-component-s-immutablejs-props-to-your-dumb-component-s-javascript-props
- * Use withImmutablePropsToJS to pass the props of this smart component, TweetListContainer, to our dumb component, TweetList
+ * Use withImmutablePropsToJS to pass the props of this smart component, TweetListContainer, to the dumb component, TweetList
  */
 export default connect(mapStateToProps)(withImmutablePropsToJS(TweetList))
